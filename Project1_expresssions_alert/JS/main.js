877bead3970457f0fb380aae92b826cb0ee4e8f8
55Unere55 <tamara.maria.iloka@gmail.com>
2022-12-01 14:25:33 +0000

    update2.0

--- Project1_expresssions_alert/JS/main.js
+++ Project1_expresssions_alert/JS/main.js
@@ -1,21 +1,21 @@
 alert ("I can see clearly now, the rain has gone");
-document.write (from Jimmy Cliff);
+document.write ("from Jimmy Cliff");
 document.write("Jerry told Mike, \"Get her now, or we will get problem!\"<br>\"Do not sweat your Shorts, I am nearly back!");
 document.write ("\"Be who you are and say what you feel,"+"because those who mind don\'t matter and those who matter don\'t mind.\""+-Dr. Seuss");
 var Family = "The Clauses" , Dad="Nicolas" , Mother="Mrs. Claus" Daughter="Holly" Son="Nick";
-document.write (Dad);
+document.write ("Dad");
 var blues="Mother";
 var blues= blues.fontcolor("blue");
-document.write (blues);
+document.write ("blues");
 var greens="Son";
 var greens=greens.fontcolor("green");
-document.write (greens)
+document.write ("greens")
 var reds="Dad";
 var reds= reds.fontcolor("red");
 document.write ("reds");
 var yellows="Daughter";
 var yellows=yellows.fontcolor("yellow");
-document.write (yellows);
+document.write ("yellows");
 Name="type Name in here";
 function my_first_Function(){
     var str = "Merry Christmas";
