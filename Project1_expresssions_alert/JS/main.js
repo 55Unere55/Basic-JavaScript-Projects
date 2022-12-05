a3d11edb33d1ad0cc24e19571769fe32fb7303f0
55Unere55 <tamara.maria.iloka@gmail.com>
2022-12-05 09:53:49 +0000

    update

--- Project1_expresssions_alert/JS/main.js
+++ Project1_expresssions_alert/JS/main.js
@@ -1,8 +1,8 @@
 alert ("I can see clearly now, the rain has gone");
 document.write ("from Jimmy Cliff");
 document.write("Jerry told Mike, \"Get her now, or we will get problem!\"<br>\"Do not sweat your Shorts, I am nearly back!");
-document.write ("\"Be who you are and say what you feel,"+"because those who mind don\'t matter and those who matter don\'t mind.\""+-Dr. Seuss");
-var Family = "The Clauses" , Dad="Nicolas" , Mother="Mrs. Claus" Daughter="Holly" Son="Nick";
+document.write ("\"Be who you are and say what you feel,"+"because those who mind don\'t matter and those who matter don\'t mind.\"+-Dr. Seuss");
+var Family = "The Clauses" , Dad="Nicolas" , Mother="Mrs. Claus" Daughter="Holly" Son="Nick"";
 document.write ("Dad");
 var blues="Mother";
 var blues= blues.fontcolor("blue");
