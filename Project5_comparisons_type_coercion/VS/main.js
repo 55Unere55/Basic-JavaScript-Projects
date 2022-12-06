a9a1624d1d67ee2ae2aac2460059fa8b5daab690
55Unere55 <tamara.maria.iloka@gmail.com>
2022-12-06 16:19:38 +0000

    update2.3

---
+++ Project5_comparisons_type_coercion/VS/main.js
@@ -0,0 +1,57 @@
+document.write(typeof "Word");
+
+function my_Function() {
+    document.getElementById("Test").innerHTML = 45/9;
+}
+
+document.getElementById("Test").innerHTML = isNaN('This is a string');
+
+document.getElementById("Test").innerHTML = isNaN('998');
+
+document.write('4E210');
+
+document.write('-4E210');
+
+document.write('10>2');
+
+document.write('10<2');
+
+console.log('2 + 2');
+
+document.write("10" + 5);
+
+document.write('20==20');
+
+document.write('5==9');
+
+X= 37;
+Y= 37;
+document.write('X===Y');
+
+X= 24;
+Y= "27";
+document.write('X===Y');
+
+A = Iloka;
+B = Iloka;
+document.write('A===B');
+
+document.write('5 > 2 && 10 > 4');
+
+document.write('5 > 10 && 10 > 4');
+
+document.write('5 > 10 || 10 > 4');
+
+document.write('5 > 10 || 10 > 20');
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(5>10);
+}
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(20>10);
+}
+
+
+
+
