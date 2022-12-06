eb9b58266ef751fca26c60cb263b18d4bcc32f54
55Unere55 <tamara.maria.iloka@gmail.com>
2022-12-06 10:23:30 +0000

    update 2.2

---
+++ Project4_dictionaries/JS/main.js
@@ -0,0 +1,11 @@
+function my_Dictionary() {
+    var Cookies ={
+        Color= light brown;
+        filling = cream;
+        design= round;
+        Pieces=500;
+    }:
+    document.getElementById("Dictionary") .innerHTML = Cookies.Pieces;
+}
+
+
